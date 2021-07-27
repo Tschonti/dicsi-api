ce045694bb18ec2604b1ebda619da901c9dd565f
Sámuel Fekete <feketesamu@gmail.com>
2021-07-27 16:33:41 +0200

    implement searching

--- hello/views.py
+++ hello/views.py
@@ -1,20 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 import requests
+from rest_framework.response import Response
 
 from rest_framework import viewsets
 
 from .serializers import SongSerializer
 from .models import Song
 
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
+
 class SongViewSet(viewsets.ModelViewSet):
     queryset = Song.objects.all().order_by('id')
     serializer_class = SongSerializer
 
-# Create your views here.
-def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
-
-
+@api_view(['GET'])
+@renderer_classes([JSONRenderer])
+def search(request, term):
+    songs = Song.objects.filter(title__icontains=term)
+    return Response(SongSerializer(songs, many=True).data)

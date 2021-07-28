8ea6fc21aa1b005225d1a8f2b25ac130b1d590e6
Sámuel Fekete <feketesamu@gmail.com>
2021-07-28 13:11:17 +0200

    sa

--- hello/urls.py
+++ hello/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
+from rest_framework.authtoken import views
 from . import views
 
 router = routers.DefaultRouter()
@@ -10,5 +11,6 @@ router.register(r'songs', views.SongViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-token-auth/', views.obtain_auth_token),
     path('search/<term>/', views.search)
 ]

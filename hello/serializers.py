72b926fb2ab1d53a657296852b7d18ce543f1c54
Sámuel Fekete <feketesamu@gmail.com>
2022-01-14 13:00:37 +0100

    playlist serializer update

--- hello/serializers.py
+++ hello/serializers.py
@@ -37,7 +37,7 @@ class PlaylistSerializer(serializers.Serializer):
     def update(self, instance, validated_data):
         instance.name = validated_data.get('name', instance.name)
         instance.created_at = validated_data.get('created_at', instance.created_at)
-        instance.songs = validated_data.get('songs', instance.songs)
+        instance.songs.set(validated_data.get('songs', instance.songs))
 
         instance.save()
         return instance

e9e2c3dab445e9bd57f636cde8e1b6ce7a192c5a
Sámuel Fekete <feketesamu@gmail.com>
2022-01-14 13:14:15 +0100

    playlist serializer update

--- hello/serializers.py
+++ hello/serializers.py
@@ -32,9 +32,8 @@ class PlaylistSerializer(serializers.Serializer):
     songs = serializers.PrimaryKeyRelatedField(queryset=Song.objects.all(), many=True, allow_empty=True, required=False)
 
     def create(self, validated_data):
-        print(validated_data)
-        pl = Playlist.objects.create(name=validated_data.name)
-        pl.songs.set(validated_data.songs)
+        pl = Playlist.objects.create(name=validated_data['name'])
+        pl.songs.set(validated_data['songs'])
         return pl
 
     def update(self, instance, validated_data):

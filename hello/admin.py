d7aab7df1abea3cbe0b315f33a95e941c6099a5b
Sámuel Fekete <feketesamu@gmail.com>
2022-01-31 13:35:51 +0100

    extra field on manytomany connector table

--- hello/admin.py
+++ hello/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Song, Playlist
+from .models import Song, Playlist, SongInPlaylist
 
 admin.site.register(Song)
 admin.site.register(Playlist)
+admin.site.register(SongInPlaylist)

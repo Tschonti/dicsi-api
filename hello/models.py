8b6402ab91d20879f79836fbecd69f6759bb5f0b
Sámuel Fekete <feketesamu@gmail.com>
2022-01-31 14:19:32 +0100

    recreate the model

--- hello/models.py
+++ hello/models.py
@@ -7,4 +7,12 @@ class Song(models.Model):
     lyrics = models.CharField(max_length=5000)
     verses = models.CharField(max_length=5000, blank=True, default='')
 
+class Playlist(models.Model):
+    name = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    songs = models.ManyToManyField(Song, through="SongInPlaylist")
 
+class SongInPlaylist(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+    place = models.IntegerField()

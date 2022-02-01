d8e039ce450fdce051ad42bef4f89b9e485eb15b
Sámuel Fekete <feketesamu@gmail.com>
2022-02-01 12:55:23 +0100

    nested serializer

--- hello/serializers.py
+++ hello/serializers.py
@@ -12,43 +12,44 @@ class SongInPlaylistSerializer(serializers.ModelSerializer):
         model = SongInPlaylist
         fields = ['place']
 
-#class SongSerializer(serializers.Serializer):
-#    id = serializers.IntegerField()
-#    title = serializers.CharField(max_length=256)
-#    lyrics = serializers.CharField(max_length=5000)
-#    order = SongInPlaylistSerializer()
-#    verses = serializers.ListField(
-#        child=serializers.CharField(max_length=5000), allow_empty=True, required=False
-#    )
-#
-#    def create(self, validated_data):
-#        return Song.objects.create(**validated_data)
-#
-#    def update(self, instance, validated_data):
-#        instance.title = validated_data.get('title', instance.title)
-#        instance.lyrics = validated_data.get('lyrics', instance.lyrics)
-#
-#        instance.save()
-#        return instance
-#
-#    def to_representation(self, instance):
-#        ret =  super().to_representation(instance)
-#        ret['verses'] = ret['lyrics'].split('###')
-#        return ret
+class SongSerializer(serializers.Serializer):
+   id = serializers.IntegerField()
+   title = serializers.CharField(max_length=256)
+   lyrics = serializers.CharField(max_length=5000)
+   place = SongInPlaylistSerializer()
+   verses = serializers.ListField(
+       child=serializers.CharField(max_length=5000), allow_empty=True, required=False
+   )
 
-class SongSerializer(serializers.ModelSerializer):
-    place = SongInPlaylistSerializer()
-    verses = serializers.ListField(
-        child=serializers.CharField(max_length=5000), allow_empty=True, required=False
-    )
+   def create(self, validated_data):
+       return Song.objects.create(**validated_data)
 
-    #def to_representation(self, instance):
-    #    ret =  super().to_representation(instance)
-    #    ret['verses'] = ret['lyrics'].split('###')
-    #    return ret
-    class Meta:
-        model = Song
-        fields = ['id', 'title', 'verses', 'place']
+   def update(self, instance, validated_data):
+       instance.title = validated_data.get('title', instance.title)
+       instance.lyrics = validated_data.get('lyrics', instance.lyrics)
+       instance.place = validated_data.get('place', instance.place)
+
+       instance.save()
+       return instance
+
+   def to_representation(self, instance):
+       ret =  super().to_representation(instance)
+       ret['verses'] = ret['lyrics'].split('###')
+       return ret
+
+# class SongSerializer(serializers.ModelSerializer):
+#     place = SongInPlaylistSerializer()
+#     verses = serializers.ListField(
+#         child=serializers.CharField(max_length=5000), allow_empty=True, required=False
+#     )
+
+#     #def to_representation(self, instance):
+#     #    ret =  super().to_representation(instance)
+#     #    ret['verses'] = ret['lyrics'].split('###')
+#     #    return ret
+#     class Meta:
+#         model = Song
+#         fields = ['id', 'title', 'verses', 'place']
 
 class PlaylistSerializer(serializers.ModelSerializer):
     #songs = serializers.PrimaryKeyRelatedField(queryset=Song.objects.all(), many=True, allow_empty=True, required=False)

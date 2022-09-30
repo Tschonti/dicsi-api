1939a5667cd09c65b909e2388ddb44b90666938b
Sámuel Fekete <feketesamu@gmail.com>
2022-09-30 15:35:03 +0000

    move to mysql from postgre :(

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE" : "django.db.backends.postgresql",
+        "ENGINE" : "django.db.backends.mysql",
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PWD'],

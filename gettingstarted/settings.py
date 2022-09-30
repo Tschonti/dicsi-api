4c140c9121e11a17611cded37561e8e3bfd3fe25
Sámuel Fekete <feketesamu@gmail.com>
2022-09-30 13:27:39 +0000

    db settings update

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -93,6 +93,11 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE" : "django.db.backends.postgresql",
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PWD'],
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

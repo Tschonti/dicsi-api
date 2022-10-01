759f6ed734038fbc52c5f3a604bd5789635b9779
Sámuel Fekete <feketesamu@gmail.com>
2022-10-01 14:15:46 +0200

    mysql search

--- hello/models.py
+++ hello/models.py
@@ -22,3 +22,15 @@ class SongInPlaylist(models.Model):
     song = models.ForeignKey(Song, on_delete=models.CASCADE, related_name='order')
     playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
     place = models.IntegerField()
+
+class Search(models.Lookup):
+    lookup_name = 'search'
+
+    def as_mysql(self, compiler, connection):
+        lhs, lhs_params = self.process_lhs(compiler, connection)
+        rhs, rhs_params = self.process_rhs(compiler, connection)
+        params = lhs_params + rhs_params
+        return 'MATCH (%s) AGAINST (%s IN BOOLEAN MODE)' % (lhs, rhs), params
+
+models.CharField.register_lookup(Search)
+models.TextField.register_lookup(Search)
